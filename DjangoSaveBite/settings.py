2b97fba6848cc592321143a3f08199d6383b0a29
MostafaAbdelmajied <99348503+MostafaAbdelmajied@users.noreply.github.com>
2025-01-24 17:36:25 +0200

    prepare for render hosting

--- DjangoSaveBite/settings.py
+++ DjangoSaveBite/settings.py
@@ -124,6 +124,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MIDDLEWARE = [
+       'django.middleware.security.SecurityMiddleware',
+       'whitenoise.middleware.WhiteNoiseMiddleware',
+   ] + MIDDLEWARE
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
